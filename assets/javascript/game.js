4af228eeaa24b9d557e8831cf3dad92f0ead045a
Scott <clearplaid@gmail.com>
2019-04-05 21:53:32 -0400

    working game-needs cleaning

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -16,45 +16,99 @@ console.log(goalNumber);
 // When the player clicks on a crystal, it will add a specific amount of points to the player's total score.
 // Each crystal should have a random hidden value between 1 - 12.
 // Your game will hide this amount until the player clicks a crystal.
-$('#crystal1').click(crystal1Clicked());
-$('#crystal2').click(crystal2Clicked());
-$('#crystal3').click(crystal3Clicked());
-$('#crystal4').click(crystal4Clicked());
+
+
+$('#crystal1').click(crystal1Clicked);
+$('#crystal2').click(crystal2Clicked);
+$('#crystal3').click(crystal3Clicked);
+$('#crystal4').click(crystal4Clicked);
+
 
 function setCrystalValue(){
     for(i = 0; i < crystalValues.length; i++){
     var crystalValue = Math.floor(Math.random() * 12 + 1);
     crystalValues[i] = crystalValue;
-    console.log(crystalValue[i]);
+    console.log(crystalValues[i]);
     }
 }
+setCrystalValue();
 // When they do click one, update the player's score counter.
 function crystal1Clicked(){
     totalScore += crystalValues[0];
+    console.log(totalScore);
+    $('#total-score').text(totalScore);
+    if (totalScore === goalNumber){
+        wins++;
+        $('#wins').text("Wins: " + wins);
+        restartGame();
+    }
+    if (totalScore > goalNumber){
+        losses++;
+        $('#losses').text("Losses: " + losses);
+        restartGame();
+    }
+
 }
 function crystal2Clicked(){
     totalScore += crystalValues[1];
+    console.log(totalScore);
+    $('#total-score').text(totalScore);
+    if (totalScore === goalNumber){
+        wins++;
+        $('#wins').text("Wins: " + wins);
+        restartGame();
+    }
+    if (totalScore > goalNumber){
+        losses++;
+        $('#losses').text("Losses: " + losses);
+        restartGame();
+    }
+
 }
 function crystal3Clicked(){
     totalScore += crystalValues[2];
+    console.log(totalScore);
+    $('#total-score').text(totalScore);
+    if (totalScore === goalNumber){
+        wins++;
+        $('#wins').text("Wins: " + wins);
+        restartGame();
+    }
+    if (totalScore > goalNumber){
+        losses++;
+        $('#losses').text("Losses: " + losses);
+        restartGame();
+    }
+
 }
 function crystal4Clicked(){
     totalScore += crystalValues[3];
+    console.log(totalScore);
+    $('#total-score').text(totalScore);
+    if (totalScore === goalNumber){
+        wins++;
+        $('#wins').text("Wins: " + wins);
+        restartGame();
+    }
+    if (totalScore > goalNumber){
+        losses++;
+        $('#losses').text("Losses: " + losses);
+        restartGame();
+    }
+
 }
-// The player wins if their total score matches the random number from the beginning of the game.
-if (totalScore === goalNumber){
-    wins++;
-}
-// The player loses if their score goes above the random number.
-if (totalScore > goalNumber){
-    losses++;
-}
+
+// The player wins if their total score matches the goalnumber from the beginning of the game.
+
+// The player loses if their score goes above the goal number.
+
 // The game restarts whenever the player wins or loses.
 function restartGame(){
-// When the game begins again, the player should see a new random number. Also, all the crystals will have four new hidden values. Of course, the user's score (and score counter) will reset to zero.
+// When the game begins again, the player should see a new goal number. Also, all the crystals will have four new hidden values. Of course, the user's score (and score counter) will reset to zero.
     totalScore = 0;
-    newGoalNumber;
-    newGemValue;
+    goalNumber = Math.floor(Math.random() * (max-min+1)+min);
+    $('#number-to-match').html(goalNumber);
+    crystalValue = setCrystalValue();
 }
 
 
